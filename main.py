b6147a99559477dc39ae41bf6f1eca590f11212e
Appselskapet.no <harald@appselskapet.no>
2022-03-12 05:28:22 +0100

    new backend api and webui

--- main.py
+++ main.py
@@ -2,11 +2,19 @@
 from pyModbusTCP.server import ModbusServer, DataBank
 from time import sleep
 from random import uniform, random
-import os, json, itertools
+import os
+import json
+import itertools
 import http.server
 import socketserver
 import threading
 from datetime import datetime
+
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
 
 WEBPORT = 8000
 MODBUSPORT = 502
@@ -16,40 +24,49 @@ MODBUS_DI_COUNT = 30
 MODBUS_HR_COUNT = 30
 MODBUS_IR_COUNT = 30
 MODBUS_PRINT_TO_TERMINAL_ENABLED = False
-httpServerThread=""
+httpServerThread = ""
+
 
 def fronius_logic(simulation_active, simulation_active_old):
     start_welding = DataBank.get_coils(0)[0]
     main_current_signal = DataBank.get_discrete_inputs(6)[0]
     simulation_active_changed = simulation_active != simulation_active_old
-    if start_welding and not main_current_signal: # if welding started and not main current signal set (used to determine if welding is active on op panel)
-            sleep(0.4)
-            DataBank.set_discrete_inputs(6, [True])
-            DataBank.set_input_registers(0, [49914])
-            print(f"Turning welding process on.")
-            print(f"setting discrete input 6 to: {DataBank.get_discrete_inputs(6)}")
-            print(f"setting input register 0 to: {DataBank.get_input_registers(0)}")
-    elif not start_welding and main_current_signal: # if welding stopped
-            DataBank.set_discrete_inputs(6, [False])
-            DataBank.set_input_registers(0, [2])
-            print(f"Turning process off: {DataBank.get_discrete_inputs(6)}")
-            
+    # if welding started and not main current signal set (used to determine if welding is active on op panel)
+    if start_welding and not main_current_signal:
+        sleep(0.4)
+        DataBank.set_discrete_inputs(6, [True])
+        DataBank.set_input_registers(0, [49914])
+        print(f"Turning welding process on.")
+        print(
+            f"setting discrete input 6 to: {DataBank.get_discrete_inputs(6)}")
+        print(
+            f"setting input register 0 to: {DataBank.get_input_registers(0)}")
+    elif not start_welding and main_current_signal:  # if welding stopped
+        DataBank.set_discrete_inputs(6, [False])
+        DataBank.set_input_registers(0, [2])
+        print(f"Turning process off: {DataBank.get_discrete_inputs(6)}")
+
     if main_current_signal:
         wirefeed_speed_command = DataBank.get_holding_registers(5)[0]
-        welding_voltage = uniform(22,24)
-        welding_current = uniform(230,250)
-        welding_wirefeed_speed = uniform(wirefeed_speed_command-1, wirefeed_speed_command+1)
-        DataBank.set_input_registers(4, [int(welding_voltage*100), int(welding_current*10), int(welding_wirefeed_speed)])
+        welding_voltage = uniform(22, 24)
+        welding_current = uniform(230, 250)
+        welding_wirefeed_speed = uniform(
+            wirefeed_speed_command-1, wirefeed_speed_command+1)
+        DataBank.set_input_registers(
+            4, [int(welding_voltage*100), int(welding_current*10), int(welding_wirefeed_speed)])
         #print(f"simulating welding with parameters: {welding_voltage} V,  {welding_current} A,  and {welding_wirefeed_speed/100} m/min.")
 
     if simulation_active_changed and simulation_active:
-        DataBank.set_holding_registers(1, [DataBank.get_holding_registers(1)[0] + 1])
+        DataBank.set_holding_registers(
+            1, [DataBank.get_holding_registers(1)[0] + 1])
     elif not simulation_active and simulation_active_changed:
-        DataBank.set_holding_registers(1, [DataBank.get_holding_registers(1)[0] - 1])
+        DataBank.set_holding_registers(
+            1, [DataBank.get_holding_registers(1)[0] - 1])
 
 def dict_to_json(filename, _dict):
     with open(filename, "w", encoding="utf-8") as f:
         return json.dump(_dict, f)
+
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
@@ -61,23 +78,83 @@ def runHttpServer():
         print("serving web ui at port", WEBPORT)
         httpd.serve_forever()
 
+
 def setupThreadAndStartWebServer():
-    httpServerThread = threading.Thread(target=runHttpServer) 
+    httpServerThread = threading.Thread(target=runHttpServer)
     httpServerThread.setDaemon(True)
     httpServerThread.start()
 
+
 def randomWord():
-    return uniform(0,65536)
+    return uniform(0, 65536)
+
 
 def randomBit():
     return random() < 0.5
 
+
+@app.route('/signals')
+def get_signal_dict():
+    holding_registers = DataBank.get_holding_registers(0, MODBUS_HR_COUNT)
+    input_registers = DataBank.get_input_registers(0, MODBUS_IR_COUNT)
+    coils = DataBank.get_coils(0, MODBUS_CO_COUNT)
+    discrete_inputs = DataBank.get_discrete_inputs(0, MODBUS_DI_COUNT)
+
+    signal_list = [{
+                "type": "holdingRegister",
+                "address": {
+                    "absolute": word_address * 16,
+                    "register": word_address},
+                "name": f"holdingRegister_{word_address}",
+                "value": value
+            } for (word_address, value) in enumerate(holding_registers)]
+
+    signal_list.extend(
+        [
+            {
+                "type": "inputRegister",
+                "address": {
+                    "absolute": word_address * 16,
+                    "register": word_address},
+                "name": f"inputRegister_{word_address}",
+                "value": value
+            } for (word_address, value) in enumerate(input_registers)])
+
+    signal_list.extend(
+        [
+            {
+                "type": "discreteInput",
+                "address": {
+                    "absolute": bit_address,
+                    "register": int(bit_address/16)},
+                "name": f"discreteInput_{bit_address}",
+                "value": value
+            } for (bit_address, value) in enumerate(discrete_inputs)])
+
+    signal_list.extend(
+        [
+            {
+                "type": "coil",
+                "address": {
+                    "absolute": bit_address,
+                    "register": int(bit_address/16)},
+                "name": f"coil_{bit_address}",
+                "value": value
+            } for (bit_address, value) in enumerate(coils)])
+
+
+    return {
+        "signals": signal_list,
+        "timestamp": datetime.now().isoformat()
+    }
+
+
 def setupAndStartModbusServer():
-    simulation_active=False
-    simulation_active_old=False
+    simulation_active = False
+    simulation_active_old = False
 
     server = ModbusServer("0.0.0.0", MODBUSPORT, no_block=True)
-    try:    
+    try:
         print("Starting modbus server...")
         server.start()
         print("Modbus server is online")
@@ -87,33 +164,24 @@ def setupAndStartModbusServer():
 
             fronius_logic(simulation_active, simulation_active_old)
 
-            holding_registers = dict(list(enumerate(DataBank.get_holding_registers(0, MODBUS_HR_COUNT))))
-            input_registers = dict(list(enumerate(DataBank.get_input_registers(0, MODBUS_IR_COUNT))))
-            coils = dict(list(enumerate(DataBank.get_coils(0, MODBUS_CO_COUNT)))) 
-            discrete_inputs = dict(list(enumerate(DataBank.get_discrete_inputs(0, MODBUS_DI_COUNT))))
-            server_data = {"coils": coils, "discreteInputs": discrete_inputs, "holdingRegisters": holding_registers, "inputRegisters": input_registers, "timestamp": datetime.now().isoformat()}
-            dict_to_json("./webui/modbus.json", server_data)
-
-            if MODBUS_PRINT_TO_TERMINAL_ENABLED:
-                os.system("cls" if os.name == "nt" else "clear")
-                print(json.dumps(bits, indent = 1), json.dumps(words, indent = 1))
-                print(f"{'Bits':<20}{'Words'}")
-                for d1, d2 in itertools.zip_longest(sorted(bits), sorted(words), fillvalue='0'): 
-                    print(f"{d1:>2}: {bits.get(d1, 'NA'):<15} {d2:>2}: {words.get(d2, 'NA')}")
-                        
-            sleep(0.1)
-            simulation_active_old=simulation_active
+#            sleep(0.1)
+            simulation_active_old = simulation_active
 
     except Exception as e:
-            print(e)
-            print("Shuting down modbus server...")
-            server.stop()
-            print("Modbus server shut down")
+        print(e)
+        print("Shuting down modbus server...")
+        server.stop()
+        print("Modbus server shut down")
+
 
 def main():
     setupThreadAndStartWebServer()
     sleep(2)
-    setupAndStartModbusServer()
+    modbusServerThread = threading.Thread(target=setupAndStartModbusServer)
+    modbusServerThread.setDaemon(True)
+    modbusServerThread.start()
+    app.run(host="0.0.0.0", port=8080)
 
 if __name__ == '__main__':
     main()
+
--- webui/index.js
+++ webui/index.js
@@ -1,21 +1,95 @@
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-		var myObj = JSON.parse(this.responseText);
-		document.getElementById("coils").innerHTML = '<pre>' + JSON.stringify(myObj["coils"], undefined, 1).replace(/[{},]/g, '') + '</pre>';
-		document.getElementById("holding-registers").innerHTML = '<pre>' + JSON.stringify(myObj["holdingRegisters"], null, 1).replace(/[{},]/g, '') + '</pre>';
-		document.getElementById("discrete-inputs").innerHTML = '<pre>' + JSON.stringify(myObj["discreteInputs"], null, 1).replace(/[{},]/g, '') + '</pre>';
-		document.getElementById("input-registers").innerHTML = '<pre>' + JSON.stringify(myObj["inputRegisters"], null, 1).replace(/[{},]/g, '') + '</pre>';
-		document.getElementById("timestamp").innerHTML = '<pre>' + JSON.stringify(myObj["timestamp"], null, 1).replace(/[{},]/g, '') + '</pre>';
+    if (this.readyState == 4 && this.status == 200) {
+        refreshPage(JSON.parse(this.responseText))
+    }
+};
 
-	}
+function signalType(signals, tableName) {
+    return `
+  <div id="customers">
+    <table class="signal-list">
+      <caption class="signal-type-title">${tableName}</caption>
+      <thead>
+        <tr>
+          <th>Register</th>
+          <th>Bit</th>
+          <th>Type</th>
+          <th>Name</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${signals.map(signal => `
+        <tr>
+          <td>${signal.address.register}</td>
+          <td>${signal.address.absolute}</td>
+          <td>${signal.type}</td>
+          <td>${signal.name}</td>
+          <td>${signal.value}</td>
+        </tr>`).join('')}
+      </tbody>
+    </table>
+   </div>
+  `
+}
+
+
+var refreshPage = function(modbusData) {
+
+	const coils = modbusData.signals.filter(signal => signal.type==="coil")
+	const holdingRegisters = modbusData.signals.filter(signal => signal.type==="holdingRegister")
+	const discreteInputs = modbusData.signals.filter(signal => signal.type==="discreteInput")
+	const inputRegisters = modbusData.signals.filter(signal => signal.type==="inputRegister")
+	const outputs = coils.concat(holdingRegisters)
+	const inputs = discreteInputs.concat(inputRegisters)
+
+	document.getElementById("app").innerHTML = `
+	<div class="timestamp">
+		<strong>Timestamp:</strong> ${modbusData.timestamp}
+	</div>
+	
+	<div class="row">
+		<div class="column signalbox">
+			${signalType(outputs, "From master to slave")}
+		</div>
+		<div class="column signalbox">
+			${signalType(inputs, "From slave to master")}
+		</div>
+	</div>
+	`;
 };
 
 
 function updatePage() {
-	xmlhttp.open("GET", './modbus.json', true);
-	xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-	xmlhttp.send();
+    xmlhttp.open("GET", 'http://localhost:8080/signals', true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send();
 }
 
-setInterval(updatePage,1000);
+
+let intervalHandle
+
+function updateGetDataInterval(element) {
+	if (element.value > 100) 
+	{
+		clearInterval(intervalHandle)
+		intervalHandle = setInterval(updatePage, element.value);
+	} else if (element.value < 100) {
+		element.value = 100;
+	}
+}
+
+
+function initApp() {
+	updatePage()
+	
+	window.addEventListener('load', function () {
+
+		intervalHandle = setInterval(updatePage, document.getElementById("update-interval").value);
+	})
+
+
+}
+
+initApp()
